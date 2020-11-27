e6312caaf9fb614d138aed81204deda2b19d284b
Artur Ayukhanov <aartbear@gmail.com>
2020-11-27 12:58:22 +0300

    Еще 2 алиаса гита
    closes https://github.com/vanessa-opensource/vanessa-bootstrap/issues/5

--- tools/git-global-init.cmd
+++ tools/git-global-init.cmd
@@ -9,10 +9,15 @@ git config --global user.email aartbear@gmail.com
 
 git config --global core.quotePath false
 
+@rem https://git-scm.com/book/en/v2/Git-Basics-Git-Aliases
+
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+
+git config --global alias.unstage 'reset HEAD --'
+git config --global alias.last 'log -1 HEAD'
 
 @rem for Windows
 
